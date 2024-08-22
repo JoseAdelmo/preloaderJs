e9ff6f3ccdcba10429a06a6eb86225b314e75c71
José Adelmo <99682808+JoseAdelmo@users.noreply.github.com>
2024-08-22 09:01:57 -0300

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -1,4 +1,3 @@
-// Create the overlay and SVG elements
 const overlay = document.createElement('div');
 overlay.id = 'loader';
 overlay.style.cssText = `
@@ -14,10 +13,10 @@ overlay.style.cssText = `
     z-index: 999;
 `;
 
-const svgImage = document.createElement('img');
-svgImage.id = 'svgImage';
-svgImage.src = 'https://jvsc99.github.io/preloaderJs/imagem.svg';
-svgImage.style.cssText = `
+const gifImage = document.createElement('img');
+gifImage.id = 'gifImage';
+gifImage.src = 'https://joseadelmo.github.io/preloaderJs/iSOW_web_preload.gif';
+gifImage.style.cssText = `
     max-width: 100%;
     max-height: 100%;
     display: none;
@@ -26,25 +25,16 @@ svgImage.style.cssText = `
     left: 50%;
     transform: translate(-50%, -50%);
 `;
-svgImage.style.display = 'none';
 
-// Append the elements to the body
+overlay.appendChild(gifImage);
 document.body.appendChild(overlay);
-document.body.appendChild(svgImage);
 
-// Function to hide the overlay and display the SVG
 function hideOverlay() {
     overlay.style.display = 'none';
-    svgImage.style.display = 'block';
+    gifImage.style.display = 'block';
 }
 
-// Add an event listener to hide the overlay when all external JS files are loaded
 document.addEventListener('DOMContentLoaded', () => {
-    // Replace the following lines with the actual code that loads your external JS files
-    // For demonstration purposes, we'll use a setTimeout to simulate loading external JS files.
-    setTimeout(hideOverlay, 100); // Replace with your actual loading code.
-});
+    setTimeout(hideOverlay, 100);
 
-// Fallback: If all external resources are loaded and the DOMContentLoaded event doesn't fire,
-// we'll still hide the overlay when the window's load event is triggered.
 window.addEventListener('load', hideOverlay);
